261dee19c1cb49d2da0578f35d92dbf939e4fc24
fsegall <segall.felipe@gmail.com>
2023-06-06 11:32:11 -0300

    Push code to be refactored

--- src/generateInvoices.ts
+++ src/generateInvoices.ts
@@ -1,20 +1,49 @@
-import pgp from "pg-promise";
+import pgp from 'pg-promise';
+import moment from 'moment';
 
 export default class GenerateInvoices {
-    async execute (input: Input): Promise<Output> {
+    async execute (input: Input): Promise<Output[]> {
         const connection = pgp()('postgres://postgres:admin123@localhost:5432/postgres');
         const contracts = await connection.query('SELECT * FROM techcompany.contract', []);
+        const output: Output[] = [];
         console.log(contracts)
+
         for(const contract of contracts) {
+
+            if (input.type === 'cash') {
+                const payments =  await connection.query('SELECT * FROM techcompany.contract WHERE id_contract = $1', [contract.id_contract]);
+                for(const payment of payments) {
+
+                    if(payment.date.getMonth() + 1 !== input.month || payment.date.getFullYear() !== input.year) continue;
+                    output.push({date: moment(payment.date).format('YYYY-MM-DD'), amount: parseFloat(payment.amount)});
+                    console.log('output', output)
+                    }
+            }
+            if (input.type === 'accrual') {
+                let period = 0;
+                while(period <= contract.period) {
+                    
+                    const date = moment(contract.date).add(period++, "months").toDate();
+                    console.log('accrual date', date)
+                    if(date.getMonth() + 1 !== input.month || date.getFullYear() !== input.year) continue;
+                    const amount = parseFloat(contract.amount)/contract.period;
+
+                    output.push({date: moment(date).format('YYYY-MM-DD'), amount})
+                }
+
+            }
             
         }
-        return [];
+        await connection.$pool.end();
+        console.log('here', output)
+        return output;
     }
 }
 
-type Input = {
+export type Input = {
     month: number,
-    year: number
+    year: number,
+    type: string
 }
 
 type Output = {
